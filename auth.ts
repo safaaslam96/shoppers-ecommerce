19be2877139d170eb4e6de32d1336939109682c9
Safa Aslam <160883400+safaaslam96@users.noreply.github.com>
2025-02-08 17:16:07 +0500

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,6 +1,18 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+    })
+  ],
+  callbacks: {
+    async redirect({url, baseUrl}) {
+      return baseUrl;
+    },
+  },
 });
+
+export default NextAuth;
